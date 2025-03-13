6956d62edde8baa7c1918f99449ab25905288a23
Francisco_Granell <franciscogranellharo@gmail.com>
2025-03-13 22:05:32 +0100

    ftp por sftp

--- functions/send_json_to_website.py
+++ functions/send_json_to_website.py
@@ -1,32 +1,36 @@
-from ftplib import FTP
+import paramiko
 import os
 from dotenv import load_dotenv
 
 
-
 def send_data():
-
+    # Cargar variables de entorno
     load_dotenv()
-    FTP_HOST = os.getenv("FTP_HOST")
-    FTP_USER = os.getenv("FTP_USER")
-    FTP_PASS = os.getenv("FTP_PASS")
+    SFTP_HOST = os.getenv("FTP_HOST")
+    SFTP_USER = os.getenv("FTP_USER")
+    SFTP_PASS = os.getenv("FTP_PASS")
 
-    # Datos del servidor FTP
-    archivo_local = "data/downbursts.json"  # Archivo que quieres subir
-    archivo_remoto = "data/downbursts.json"  # Nombre en el servidor
+    # Archivos
+    archivo_local = "data/downbursts.csv"
+    archivo_remoto = "/home/downburs/www/form/data/downbursts.csv"
 
-    # Conectar al servidor FTP
-    ftp = FTP(FTP_HOST)
-    ftp.login(FTP_USER, FTP_PASS)
+    try:
+        # Establecer conexiÃ³n
+        transport = paramiko.Transport((SFTP_HOST, 22))
+        transport.connect(username=SFTP_USER, password=SFTP_PASS) 
 
-    # Abrir el archivo en modo binario y subirlo
-    with open(archivo_local, "rb") as file:
-        ftp.storbinary(f"STOR {archivo_remoto}", file)
+        sftp = paramiko.SFTPClient.from_transport(transport)
 
-    # Cerrar la conexión
-    ftp.quit()
+        # Subir el archivo JSON
+        sftp.put(archivo_local, archivo_remoto)
+        print(f"? Archivo '{archivo_local}' subido correctamente a '{archivo_remoto}'.")
 
-    print(f"Archivo '{archivo_local}' subido como '{archivo_remoto}'")
+        # Cerrar conexiÃ³n
+        sftp.close()
+        transport.close()
+
+    except Exception as e:
+        print(f"? Error al enviar el archivo: {e}")
 
 
 if __name__ == "__main__":
